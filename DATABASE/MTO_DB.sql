dbee32c9ec0cd56a7f14d3f997828c66037159fa
mnapolitano61 <napolitano.maddalena@hotmail.com>
2018-01-27 12:25:50 +0100

    Caricamento database aggiornato

--- DATABASE/MTO_DB.sql
+++ DATABASE/MTO_DB.sql
@@ -5,17 +5,17 @@ USE MTO_DB;
 
 DROP TABLE IF EXISTS utente;
 CREATE TABLE IF NOT EXISTS utente (
-  username varchar(15) not NULL,
-  password varchar(20) not NULL,
-  email varchar(50) unique not NULL,
+  username varchar(16) not NULL,
+  password varchar(26) not NULL,
+  email varchar(50) not NULL,
   tipo varchar(40) not null, 
-  nome varchar(20) not NULL,
-  cognome varchar(20) not NULL,
+  nome varchar(26) not NULL,
+  cognome varchar(26) not NULL,
   data_nascita date, 
   annoImmatricolazione varchar(9),
   CFU int(10),
   universita varchar(50),
-  dipartimento varchar(25),
+  dipartimento varchar(45),
   azienda varchar(30),
   primary key (username),
   unique(username)
@@ -24,14 +24,16 @@ CREATE TABLE IF NOT EXISTS utente (
   
 DROP TABLE IF EXISTS tirocinio;
 CREATE TABLE IF NOT EXISTS tirocinio(
- codiceID int(25) not null,
+ codiceID int(25) not null auto_increment,
  rif_utente varchar(15) not null,
- azienda varchar(20) not NULL,
+ rif_TA varchar(15) not null,
+ rif_TE varchar(15) not null,
+ azienda varchar(21) not NULL,
  data_inizio date not null,
  data_fine date not null,
- luogo varchar(20) not null,
- tematica varchar(50) not null,
- descrizione varchar(150) not null,
+ luogo varchar(26) not null,
+ tematica varchar(51) not null,
+ descrizione varchar(151) not null,
  primary key (codiceID),
  foreign key(rif_utente) references utente(username)
  
@@ -59,10 +61,20 @@ foreign key(rif_utente) references utente(username),
 foreign key(rif_documento) references documento(codiceID)
 );
   
-  
-INSERT INTO utente VALUES ('r.napo12', 'ciao', 'r.napo12@studenti.unisa.it', 'studente', 'Rita', 'Napo', '1996-05-27', '2009/2010', '102', 'Unisa', 'Matematica', '');
+/*STUDENTI*/  
+INSERT INTO utente VALUES ('r.napo12', 'ciao', 'r.napo1@studenti.unisa.it', 'studente', 'Rita', 'Napo', '1996-05-27', '2009/2010', 102, 'Unisa', 'Matematica', '');
+INSERT INTO utente VALUES ('m.pelu2', 'mpeluso', 'm.pelu2@studenti.unisa.it', 'studente', 'Maurizio', 'Peluso', '1994-09-22', '2009/2010', 110, 'Unisa', 'Matematica', '');
+INSERT INTO utente VALUES ('responsabile', 'ciao', 'resp@studenti.unisa.it', 'responsabileAzienda', 'Rosa', 'Peluso', '1996-05-27', '', 0,  '', '', 'Unicornilandia');
 
-select * from utente;
+/*TIROCINIO*/
+INSERT INTO tirocinio VALUES (01 , 'responsabile', 'rif_TA', 'rif_TE','Sony', '2017-12-12', '2017-03-03', 'Napoli', 'Basi dati', 'non lo so cosa devo scrivere. questa è una prova!');
+INSERT INTO tirocinio VALUES (02 , 'responsabile', 'rif_TA','rif_TE','Pony', '2017-12-12', '2017-03-03', 'Napoli', 'Sicurezza pony', 'non lo so cosa devo scrivere. questa è una prova!');
+
+SELECT * FROM utente;
+
+SELECT username FROM utente WHERE username ='r.napo12' AND password = 'ciao';
+/**/ 
+select * from tirocinio;
 /*  utente (
   username varchar(15) not NULL,
   password varchar(20) not NULL,
